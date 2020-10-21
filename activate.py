317d08a8b4e96dc9a9cf23bfa04f00abb960bb3a
Bryan <bryanegbert561@gmail.com>
2020-10-21 19:56:33 +0700

    Adding core function

--- activate.py
+++ activate.py
@@ -1,3 +1,44 @@
 import pyautogui
 import subprocess
+import time
+import sys
+import os
 
+try:
+    """This function open the file in read mode if it exist. If the file not exist, create the file"""
+    with open('list.txt', 'r') as file:
+        # Read the file content and split it into list
+        content = file.read()
+        splitted_content = content.split('\n')
+        print(splitted_content)
+        
+        # Open discord app
+        subprocess.Popen(os.environ[r"DISCORD_PATH"])
+
+        # Locate the server logo and click it
+        logo_location = pyautogui.locateCenterOnScreen(r".\images\server_logo.png")
+        pyautogui.click(logo_location)
+
+        # Locate voice channel and click it
+        voice_channel_location = pyautogui.locateCenterOnScreen(r".\images\voice_channel.png")
+        pyautogui.click(voice_channel_location)
+
+        # Locate the chatbox and click it
+        chatbox_location = pyautogui.locateCenterOnScreen(r".\images\chatbox.png")
+        pyautogui.click(chatbox_location)
+
+        # Looping over the list and write the content.
+        # If it done looping all the list, stop the program
+        for i in splitted_content:
+            if i != '':
+                pyautogui.write(f"-play {i}")
+                pyautogui.press('enter')
+            else:
+                continue
+        else:
+            pyautogui.write("-lq")
+            sys.exit()
+
+except FileNotFoundError:
+    with open('list.txt', 'w') as file:
+        pass
