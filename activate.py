6b0190f3318e6dc21c27d3a98b579e6e01ea1386
Bryan <bryanegbert561@gmail.com>
2020-10-27 13:57:40 +0700

    Enhance error handling

--- activate.py
+++ activate.py
@@ -1,9 +1,21 @@
-import pyautogui, subprocess, shutil, click, time, sys, cv2, os
+import pyautogui
+import subprocess
+import shutil
+import click
+import time
+import sys
+import cv2
+import os
 
 # Image directory
 vc_image_dir = ".\\images\\voice_channel"
 server_image_dir = ".\\images\\server_img"
 chat_image_dir = ".\\images\\chat_channel"
+
+# File inside the image directory
+server_img = os.listdir(server_image_dir)
+vc_img = os.listdir(vc_image_dir)
+chat_img = os.listdir(chat_image_dir)
 
 
 @click.group()
@@ -19,20 +31,36 @@ def cli():
 def main(prefix, vc, chat, server):
     """The main function"""
     try:
-        with open('list.txt', 'r', encoding="cp437", errors="ignore") as file:
-            # Read the file content and split it into list
-            content = file.read()
-            splitted_content = content.split('\n')
+        """Validate if the file in image directory exist of not"""
 
+        # Checking the file in .\images\server_img directory
+        os.path.isfile(
+            f"{server_image_dir}\\{server_img[server-1]}")
+
+        # Checking file in .\images\voice_channel directory
+        os.path.isfile(
+            f"{vc_image_dir}\\{vc_img[vc-1]}")
+
+        # Checking file in .\images\chat_channel directory
+        os.path.isfile(
+            f"{chat_image_dir}\\{chat_img[chat-1]}")
+
+        # Checking the chatbox image
+        os.path.isfile(
+            ".\\images\\chatbox.png")
+
+    except FileNotFoundError:
+        click.echo("File not found. Create file. Creating 'list.txt'...")
+        with open('list.txt', 'w') as file:
+            click.echo('done!')
+
+    except IndexError:
+        click.secho('ERROR: Image file not available. Use the view command to see the file available or --help command to see available commands', fg='bright_red')
+    else:
+        try:
             # Open discord app
             subprocess.Popen(os.environ["DISCORD_PATH"])
             time.sleep(2)
-
-            server_img = os.listdir(server_image_dir)
-            vc_img = os.listdir(vc_image_dir)
-            chat_img = os.listdir(chat_image_dir)
-
-            # Locate the server logo and click it
             logo_location = pyautogui.locateCenterOnScreen(
                 f"{server_image_dir}\\{server_img[server-1]}", confidence=0.6)
             pyautogui.click(logo_location)
@@ -54,30 +82,34 @@ def main(prefix, vc, chat, server):
                 ".\\images\\chatbox.png", confidence=0.3)
             pyautogui.click(chatbox_location)
 
-            # Looping over the list and write the content.
-            # If it done looping all the list, stop the program
-            for i in splitted_content:
-                if i != '':
-                    pyautogui.write(f"{prefix}play {i}", interval=0.03)
+            with open('list.txt', 'r', encoding="cp437", errors="ignore") as file:
+                # Read the file content and split it into list
+                content = file.read()
+                splitted_content = content.split('\n')
+                # Looping over the list and write the content.
+                # If it done looping all the list, stop the program
+                for i in splitted_content:
+                    # Check for empty string e.g.line break 
+                    if i != '':
+                        pyautogui.write(f"{prefix}play {i}", interval=0.03)
+                        pyautogui.press('enter')
+                        time.sleep(0.02)
+                    # If exist, skip it and continue the loop
+                    else:
+                        continue
+                else:
+                    # Enter loop queue command.
+                    # If the command prefix is "-"(Groovy musicbot)
+                    if (prefix == "-"):
+                        pyautogui.write(f"{prefix}loop queue")
+                    # If the command prefix is "!"(Rythm musicbot)
+                    else:
+                        pyautogui.write(f"{prefix}loopqueue")
                     pyautogui.press('enter')
-                    time.sleep(0.02)
-                else:
-                    continue
-            else:
-                if (prefix == "-"):
-                    pyautogui.write(f"{prefix}loop queue")
-                else:
-                    pyautogui.write(f"{prefix}loopqueue")
-                pyautogui.press('enter')
-                click.secho('done!', fg='green')
-                sys.exit()
-
-    except FileNotFoundError:
-        click.echo("File not found. Create file. Creating 'list.txt'...")
-        with open('list.txt', 'w') as file:
-            click.echo('done!')
-    except pyautogui.FailSafeException:
-        click.echo('Execution has been stopped')
+                    click.secho('done!', fg='green')
+                    sys.exit()
+        except pyautogui.FailSafeException:
+            click.secho('Execution has been stopped', fg='yellow')
 
 
 @cli.command()
